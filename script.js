f2ecc617de7ea68f1c17e6e12bc89ed0e8a914ed
BPouliot1 <150730804+BPouliot1@users.noreply.github.com>
2024-01-17 17:58:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,62 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChars = "0123456789";
+var symbolsChars = "!@#$%^&*()_+-=";
+let password = "";
+let allowedChars = "";
+
 // Write password to the #password input
 function writePassword(password) {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols){
-  
-  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const numberChars = "0123456789";
-  const symbolsChars = "!@#$%^&*()_+-=";
-  let password = "";
-  let allowedChars = "";
-  letpassword = "";
+  var passwordLength = prompt("Please insert password between 8-128");
+  if(passwordLength < 8) {
+    alert("Password too short");
+    return;
+  }
+  if(passwordLength > 128) {
+    alert("Password too long");
+    return;
+  }
 
-  allowedChars += includeLowercase ? lowercaseChars : "";
-  allowedChars += includeUppercase ? uppercaseChars : "";
-  allowedChars += includeNumbers ? numberChars : "";
-  allowedChars += includeSymbols ? symbolsChars : "";
-
-  if(length <= 0){
-    return "(password length must be at least 8)"
-   }
-   if(allowedChars.length === 0){
-    return "(password length must be at least 1)"
-   }
-   for(let i = 0; i < length; i ++){
-    const randomIndex = Math.floor(Math.random() * allowedChars.length);
-    password += allowedChars[randomIndex];
-
-   }
-  return password;
+var upperCase = window.prompt("Would you like use Uppercase letters");
+if(upperCase) {
+  userSelection.push(upperCase);
+}
+var lowerCase = window.prompt("Would you like use Lowercase letters");
+if(lowerCase) {
+  userSelection.push(lowerCase);
 }
 
-const passwordLength = 10
-const includeLowercase = true;
-const includeUppercase = true;
-const includeNumbers = true;
-const includeSymbols = true;
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+var numbers = window.prompt("Would you like to use Numbers");
+if(numbers) {
+  userSelection.push(numbers);
+}
 
-const password = generatePassword(passwordLength, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
+var symbols = window.prompt("Would you like to use Symbols");
+if(symbols) {
+  userSelection.push(symbols);
+}
+
+console.log("user selection = " + userSelection);
+if (userSelection.length === 0)
+return;
+
+  for(let i = 0; i < length; i ++){
+    var randomIndex = Math.floor(Math.random() * userSelection);
+    var random = userSelection.charAt(randomIndex);
+  }
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword,);
+
 writePassword(password);
 console.log("Write password: ${password}");
+
